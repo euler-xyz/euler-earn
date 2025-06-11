85fc92995763a88fb62e8e932ef0c338021ac55e
dglowinski <dariushg32111@gmail.com>
2025-06-11 15:37:45 +0200

    wip tests

--- test/MulticallTest.sol
+++ test/MulticallTest.sol
@@ -1,21 +1,21 @@
-// // SPDX-License-Identifier: GPL-2.0-or-later
-// pragma solidity ^0.8.26;
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.26;
 
-// import "./helpers/IntegrationTest.sol";
+import "./helpers/IntegrationTest.sol";
 
-// contract MulticallTest is IntegrationTest {
-//     bytes[] internal data;
+contract MulticallTest is IntegrationTest {
+    bytes[] internal data;
 
-//     function testMulticall() public {
-//         data.push(abi.encodeCall(IEulerEarnBase.setCurator, (address(1))));
-//         data.push(abi.encodeCall(IEulerEarnBase.setIsAllocator, (address(1), true)));
-//         data.push(abi.encodeCall(IEulerEarnBase.submitTimelock, (ConstantsLib.MAX_TIMELOCK)));
+    function testMulticall() public {
+        data.push(abi.encodeCall(IEulerEarnBase.setCurator, (address(1))));
+        data.push(abi.encodeCall(IEulerEarnBase.setIsAllocator, (address(1), true)));
+        data.push(abi.encodeCall(IEulerEarnBase.submitTimelock, (ConstantsLib.MAX_TIMELOCK)));
 
-//         vm.prank(OWNER);
-//         vault.multicall(data);
+        vm.prank(OWNER);
+        vault.multicall(data);
 
-//         assertEq(vault.curator(), address(1));
-//         assertTrue(vault.isAllocator(address(1)));
-//         assertEq(vault.timelock(), ConstantsLib.MAX_TIMELOCK);
-//     }
-// }
+        assertEq(vault.curator(), address(1));
+        assertTrue(vault.isAllocator(address(1)));
+        assertEq(vault.timelock(), ConstantsLib.MAX_TIMELOCK);
+    }
+}
