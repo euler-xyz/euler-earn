eab4f77553a00168e3ce50432c21d22b553f4ac0
kasperpawlowski <kasperpawlowski@gmail.com>
2025-10-14 19:35:03 +0200

    fix: import paths

--- certora/harnesses/ERC20Helper.sol
+++ certora/harnesses/ERC20Helper.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.26;
 
-import {SafeERC20, IERC20} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import {SafeERC20, IERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract ERC20Helper {
     using SafeERC20 for IERC20;
